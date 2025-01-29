3c89c3fc786a4326d9a420dac0c618057d0e458e
Abolarin Emmanuel Olawale <72341638+Abolarin820@users.noreply.github.com>
2025-01-29 15:10:14 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,11 +14,12 @@ const port = process.env.PORT || 8080
 //Middleware
 app.use(express.json())
 app.use(cookiesParser())
+app.use(cors())
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials:true
-}))
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials:true
+// }))
 
 
 
